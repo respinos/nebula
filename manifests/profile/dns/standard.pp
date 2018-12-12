8a2fcd697c020bb4750fd14ce173638e2e9bbe55
Ryan Rotter <rrotter@umich.edu>
2018-12-12 16:55:42 -0600

    AWS: Set search domain via dhclient, don't set dns servers

--- manifests/profile/dns/standard.pp
+++ manifests/profile/dns/standard.pp
@@ -9,8 +9,29 @@
 # @example
 #   include nebula::profile::dns::standard
 class nebula::profile::dns::standard {
-  class { 'resolv_conf':
-    nameservers => lookup('nebula::resolv_conf::nameservers'),
-    searchpath  => lookup('nebula::resolv_conf::searchpath'),
+  exec { 'restart_networking':
+    command     => '/bin/systemctl restart networking',
+    refreshonly => true,
+  }
+
+  $searchpaths = lookup('nebula::resolv_conf::searchpath')
+  $searchpath_str = join($searchpaths, ' ')
+  if $facts['ec2_metadata'] {
+    file_line {
+      default:
+        path   => '/etc/dhcp/dhclient.conf',
+        notify => Exec['restart_networking'],
+      ;
+      'search_domain':
+        line => "supersede domain-search \"${searchpath_str}\";",
+      ;
+      'domain_name':
+        line => 'supersede domain-name "";',
+    }
+  } else {
+    class { 'resolv_conf':
+      nameservers => lookup('nebula::resolv_conf::nameservers'),
+      searchpath  => $searchpaths,
+    }
   }
 }
